4167b0367838d1970ba6b6fc79a8802ae791f76b
rkepha <kephaokari@gmail.com>
2018-06-19 21:52:26 +0300

    added better forms

--- tablerapp/views.py
+++ tablerapp/views.py
@@ -19,31 +19,38 @@ def index(request):
 
 
 
+#
+# def create_table(request):
+#
+#     if request.method == 'POST':
+#             form = ScheduleForm(request.POST,request.FILES)
+#
+#
+#             if form.is_valid():
+#                 day = form.cleaned_data['day']
+#                 course = form.cleaned_data['course']
+#                 period = form.cleaned_data['period']
+#                 room1 = form.cleaned_data['room1']
+#                 room2 = form.cleaned_data['room2']
+#                 room3 = form.cleaned_data['room3']
+#                 room4 = form.cleaned_data['room4']
+#                 form1 = Schedule(day = day, course = course, year1 = '1', year2 ='2' ,year3 = '3' ,year4 = '4',period = period ,room1 = room1,room2 = room2,room3 = room3,room4 = room4)
+#                 form1.save()
+#                 return redirect(index)
+#
+#     else:
+#         form = ScheduleForm()
+#     return render(request, 'create-table.html',{"form":form})
 
 def create_table(request):
-
     if request.method == 'POST':
-            form = ScheduleForm(request.POST,request.FILES)
+        data = request.POST['variable']
+        data1 = request.POST['variable2']
+        form1 = Schedule(day = 'Monday', course = 'ct', year1 = '1', year2 ='2' ,year3 = '3' ,year4 = '4',period = '7' ,room1 = '100',room2 = '120',room3 = data,room4 = data1)
+        form1.save()
+        return redirect(index)
+    return render(request, 'create-table.html')
 
-            if form.is_valid():
-                day = form.cleaned_data['day']
-                course = form.cleaned_data['course']
-                # year1 = form.cleaned_data['year1']
-                # year2 = form.cleaned_data['year2']
-                # year3 = form.cleaned_data['year3']
-                # year4 = form.cleaned_data['year4']
-                period = form.cleaned_data['period']
-                room1 = form.cleaned_data['room1']
-                room2 = form.cleaned_data['room2']
-                room3 = form.cleaned_data['room3']
-                room4 = form.cleaned_data['room4']
-                form1 = Schedule(day = day, course = course, year1 = '1', year2 ='2' ,year3 = '3' ,year4 = '4',period = period ,room1 = room1,room2 = room2,room3 = room3,room4 = room4)
-                form1.save()
-                return redirect(index)
-
-    else:
-        form = ScheduleForm()
-    return render(request, 'create-table.html',{"form":form})
 
 
 # @login_required(login_url='/accounts/login/')
